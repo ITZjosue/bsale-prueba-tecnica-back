f9e77675f95441d9be8f7c96117e6189c73066b0
Josue Lamas <itzjosue99@gmail.com>
2022-02-06 21:13:46 -0500

    make it better

--- src/server.js
+++ src/server.js
@@ -6,6 +6,14 @@ const cors = require('cors')
 server.use(cors())
 server.use(express.json())
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://back-1.herokuapp.com/'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 
 server.use('/api',routes)
 server.get('/',(req,res)=>res.send("<h1>Welcome to the ApiREST</h1>"))
